6cbf33a49d444082f003d983e49ddb59bcb6584c
Ashwini Gupta <ashwinigpt1996@gmail.com>
2022-04-27 00:31:04 +0530

    block-BNaacv

--- block-BNaacv/userDiary/routes/users.js
+++ block-BNaacv/userDiary/routes/users.js
@@ -7,12 +7,28 @@ router.get('/', (req, res) => {
     res.render('users', { user: ["Ashwini", "Vandana", "Ravi", "Abhishek"]});
 });
 
-router.get('/id', (req, res) => {
+router.get('/:id', (req, res) => {
     res.render('singleUser', {name: "Suraj", email: "suraj123@gmail.com"});
 });
 
 router.get('/new', (req, res) => {
     res.send(req.body);
 });
+
+router.post('/', (req, res) => {
+
+});
+
+router.get('/:id/edit', (req, res) => {
+
+});
+
+router.put('/:id', (req, res) => {
+
+});
+
+router.delete('/:id', (req, res) => {
+
+})
 
 module.exports = router;
