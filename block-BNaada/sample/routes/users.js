fc34ea7cc4601e7a5253f442e223e46bc9e22605
Ashwini Gupta <ashwinigpt1996@gmail.com>
2022-04-28 18:09:57 +0530

    block-BNaada

--- block-BNaada/sample/routes/users.js
+++ block-BNaada/sample/routes/users.js
@@ -3,7 +3,11 @@ var router = express.Router();
 var User = require('../models/User');
 
 router.get('/', (req, res) => {
-    res.render('users.ejs');
+    User.find({}, (err, users) => {
+        if(err) return next(err);
+        res.render('users.ejs', { users : users});
+    })
+    
 });
 
 router.get('/new', (req, res) => {
@@ -15,6 +19,14 @@ router.post('/users', (req, res) => {
         if(err) return res.redirect('/users/new');
         res.redirect('/');
     });
+});
+
+router.get('/:id', (req, res, next) => {
+    var id = req.params.id;
+    User.findById(id, (err, user) => {
+        if(err) return next(err);
+        res.render('singleUser.ejs', {user})
+    })
 });
 
 module.exports = router;
